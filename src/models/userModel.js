0a576c99b51473336ead8c5dbc20b26321c3fcd7
functionup-am <akshaymakwana350@gmail.com>
2022-06-05 22:17:23 +0530

    demo

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,29 +1,22 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+const bookSchema = new mongoose.Schema( {
+    bookName: String,
+    authorName: String,
+    category: {
+        type: String,
+        unique: true,
+        required: false
+    },
+    year: {
         type: String,
         unique: true,
         required: true
-    },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    }
+    
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('Books', bookSchema) //books
 
 
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,7 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
+router.post("/books", UserController.createBooks  )
+router.get("/getBooksData", UserController.getBooksData)
 
 module.exports = router;
