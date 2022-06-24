a360fb4e652e0455d14af0f97080e8fff3a5df5d
DonIndra95 <royindrashish95@gmail.com>
2022-06-24 10:57:54 +0530

    Bugs fix

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-mongoose
-  .connect(
-    "mongodb+srv://IndrashishRoy:windows10@radon-cohort-cluster.gtmdsvp.mongodb.net/groupAADI-DB?retryWrites=true&w=majority",
+mongoose.connect("mongodb+srv://IndrashishRoy:windows10@radon-cohort-cluster.gtmdsvp.mongodb.net/groupAADI-DB?retryWrites=true&w=majority",
     {
       useNewUrlParser: true,
     }
